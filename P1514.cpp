0ac2aa69141669c59df8bb1d441929ef11d36cdb
Ubuntu <psaroj@ubuntu.v3czi3jhblwuxdwu3xnflvzzxc.rx.internal.cloudapp.net>
2024-08-27 17:35:55 +0000

    27 aug sol

--- P1514.cpp
+++ P1514.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+
+#define INF 0x3f3f3f3f
 
 class Dijkstra
 {
@@ -23,6 +24,27 @@ public:
 
     double findShortestPath(int src, int dst)
     {
+        priority_queue<pair<double, int>, vector<pair<double, int>>, greater<pair<double, int>>> Q;
+        vector<double> dist(this->n, INF);
+
+        Q.push(pair(0, src));
+        dist[src] = 0;
+
+        while (!Q.empty())
+        {
+            int u = Q.top().second;
+            Q.pop();
+
+            for (auto neighbor : adj[u])
+            {
+                if (dist[neighbor.first] > dist[u] + neighbor.second)
+                {
+                    dist[neighbor.first] = dist[u] + neighbor.second;
+                    Q.push(pair(dist[neighbor.first], neighbor.first));
+                }
+            }
+        }
+        return dist[dst];
     }
 };
 
@@ -35,7 +57,7 @@ public:
         vector<vector<int>> transEdges;
         for (int i = 0; i < edges.size(); i++)
         {
-            if (succProb[i] > 0.0)
+            if (succProb[i] > 0)
             {
                 transEdges.push_back(edges[i]);
                 transWeights.push_back(abs(log(succProb[i])));
@@ -51,4 +73,13 @@ public:
 
 int main()
 {
+    int n = 3;
+    vector<vector<int>> edges = {{0, 1}, {1, 2}, {0, 2}};
+    vector<double> succProb = {0.5, 0.5, 0.2};
+    Solution sol;
+
+    int start = 0, end = 2;
+    cout << sol.maxProbability(n, edges, succProb, start, end) << endl;
+
+    return 0;
 }
