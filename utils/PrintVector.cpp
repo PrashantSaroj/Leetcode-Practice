2ffa83a86222d88fca2be3a50c61b15cb7409603
PrashantSaroj <prashantsaroj2006@gmail.com>
2024-09-05 06:59:27 +0000

    template utility for print vector of printable type

---
+++ utils/PrintVector.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+void printVector(vector<T> v) {
+    for (T &t : v) {
+        cout << t << " ";
+    }
+    cout << endl;
+}
