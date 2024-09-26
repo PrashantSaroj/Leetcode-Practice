0c49d1acd1531466153d820a91db83cc83bd67d1
PrashantSaroj <prashantsaroj2006@gmail.com>
2024-09-26 09:32:01 +0000

    Daily Practice: 26th Sept

---
+++ cpp/P729.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> interval;
+
+class MyCalendar {
+    vector<interval> events;
+
+    bool intersect(interval i1, interval i2) {
+        if (i1.first > i2.first) {
+            return intersect(i2, i1);
+        }
+        return !(i1.second <= i2.first);
+    }
+
+   public:
+    MyCalendar() {
+    }
+
+    bool book(int start, int end) {
+        interval currEvent(start, end);
+        for (auto event : events) {
+            if (intersect(event, currEvent)) {
+                return false;
+            }
+        }
+        events.push_back(currEvent);
+        return true;
+    }
+};
+
+int main() {
+    return 0;
+}
