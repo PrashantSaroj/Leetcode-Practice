c7d526ea08ac76d3659045d5a9c3896b44fef6dc
PrashantSaroj <prashantsaroj2006@gmail.com>
2024-12-09 21:23:24 +0000

    Daily Practice: 7th Dec

---
+++ cpp/P1760.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<vector<int>, int> tc_type;
+
+class Solution {
+   public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+    }
+};
+
+int main() {
+    vector<tc_type> testCases = {
+        {{9}, 2},
+        {{2, 4, 8, 2}, 4},
+    };
+    return 0;
+}
